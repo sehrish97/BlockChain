68d2ebe59bb31d21b3c6302e749106d0db7a1511
SEHRISH BIBI <57295611+sehrish97@users.noreply.github.com>
2022-08-28 05:03:04 -0700

    Update Task2.sol

--- Task2.sol
+++ Task2.sol
@@ -12,26 +12,20 @@ pragma solidity >=0.7.0 <0.9.0;
  }
  contract Task2{
      function setnum(uint a,uint b) public pure returns(uint){
-        // uint a=20;
-        // uint b=10;
-         uint sum=a+b;
-         uint diff= a-b;
-         return(sum-diff);
+         return(a+b)-(a-b);
 
      }
  }
 contract Task3{
     function find(int256 a) public pure returns(int256){
         require(a>0 ,'a not be negative');
-        int256 remainder = a%3;  
-        return remainder;
+        return a%3;
     }
 }
 
 contract Task4{
     function average(int256 a,int256 b,int256 c) public pure returns(int256){
         require(a>0 && b>0 && c>0,'plz enter a positive number');
-        int256 avg=((a+b+c)/3);
-        return(avg);
+        return((a+b+c)/3);
     }
 }
